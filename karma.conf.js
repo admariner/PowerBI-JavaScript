ad7bf5aadbee40c752f01cb8535357c5ce854512
Lakshmi Satya Sai Kumar Pasupureddi (Accenture International Limited) <v-lpasupured@microsoft.com>
2025-04-14 05:06:20 +0000

    Merged PR 717923: Onboarding Sample UnitTest to Enable code coverage and added azurepipelines-coverage.yml for QCS requirement- powerbi-javascript
    As part of this PR, unit tests have been migrated to the buddy pipeline from this classic pipeline - https://dev.azure.com/powerbi/Embedded/_build?definitionId=6720&_a=summary
    
    Also, code coverage has been onboarded and enabled. Please refer to the files changed for detailed info.
    
    Development Test Run:
    https://dev.azure.com/powerbi/Embedded/_build/results?buildId=13760700&view=results
    
    CodeCoverage Tab:
    https://dev.azure.com/powerbi/Embedded/_build/results?buildId=13760700&view=codecoverage-tab
    
    #### AI description  (iteration 1)
    #### PR Classification
    New feature to enable code coverage and integrate with Azure Pipelines for QCS requirement.
    
    #### PR Summary
    This pull request introduces code coverage capabilities and integrates them with Azure Pipelines.
    - `.pipelines/OneBranch.Buddy.yml`: Added steps to install Puppeteer, run Karma tests, and publish test and code coverage results.
    - `karma.conf.js`: Configured Karma to include coverage and TypeScript support, added custom Chrome headless launcher.
    - `tsconfig.json`: Included `test/**/*.ts` files for TypeScript compilation.
    - `azurepipelines-coverage.yml`: Added new file to define code coverage targets and status reporting.
    - `package.json`: Added `karma-coverage` dependency.
    <!-- GitOpsUserAgent=GitOps.Apps.Server.pullrequestcopilot -->
    
    Related work items: #1657919

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,10 @@
 var argv = require('yargs').argv;
 
-var browserName = 'Chrome_headless';
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+var browserName = 'ChromeHeadlessCustom';
 if (argv.chrome) {
-  browserName = 'Chrome_headless'
+  browserName = 'ChromeHeadlessCustom'
 }
 else if (argv.firefox) {
   browserName = 'Firefox'
@@ -12,18 +14,27 @@ const flags = [
   '--no-proxy-server',
   '--js-flags="--max_old_space_size=6500"',
   '--high-dpi-support=1',
+  '--no-sandbox',
+  '--headless',
+  '--disable-gpu',
+  '--window-size=800,800',
 ];
 module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine','karma-typescript'],
     files: [
       './node_modules/jquery/dist/jquery.js',
       './node_modules/es6-promise/dist/es6-promise.js',
-      './tmp/**/*.js',
+      { pattern: './src/**/*.ts' },
+      { pattern: './test/**/*.ts' },
       { pattern: './test/**/*.html', served: true, included: false }
     ],
     exclude: [],
-    reporters: argv.chrome ? ['kjhtml'] : ['spec', 'junit'],
+    preprocessors: {
+      './src/**/*.ts': ['karma-typescript'],
+      './test/**/*.ts': ['karma-typescript']
+    },
+    reporters: argv.chrome ? ['kjhtml'] : ['spec', 'karma-typescript', 'progress', 'junit', 'coverage'],
     autoWatch: true,
     browsers: [browserName],
     browserNoActivityTimeout: 300000,
@@ -33,18 +44,37 @@ module.exports = function (config) {
       'karma-jasmine',
       'karma-spec-reporter',
       'karma-jasmine-html-reporter',
-      'karma-junit-reporter'
+      'karma-junit-reporter',
+      'karma-coverage',
+      'karma-typescript'
     ],
     customLaunchers: {
-      'Chrome_headless': {
+      'ChromeHeadlessCustom': {
         base: 'Chrome',
         flags: flags.concat("--no-sandbox", "--window-size=800,800"),
       },
     },
     junitReporter: {
-      outputDir: 'tmp',
+      outputDir: 'test-results',
       outputFile: 'testresults.xml',
       useBrowserName: false
+    },
+    coverageReporter: {
+      dir: 'coverage',
+      reporters: [
+        { type: 'cobertura', subdir: '.', file: 'cobertura-coverage.xml' },
+        { type: 'html', subdir: 'html' }
+      ]
+    },
+    karmaTypescriptConfig: {
+      tsconfig: './tsconfig.json',
+      reports: {
+        "cobertura": {
+          "directory": "coverage",
+          "filename": "cobertura-coverage.xml"
+        },
+        "html": "coverage/html"
+      }
     },
     retryLimit: 0,
     logLevel: argv.debug ? config.LOG_DEBUG : config.LOG_INFO,
